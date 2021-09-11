b659a956df5d24419cbdb251baa7b2f920de846e
Michel Robijns <michelrobijns@users.noreply.github.com>
2021-09-11 20:27:11 +0200

    Add exercise 2.8 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([{ name: 'Arto Hellas' }]);
+  const [ persons, setPersons ] = useState([{ name: 'Ada Lovelace', number: '39-44-5323523' }]);
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -10,12 +11,16 @@ const App = () => {
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -26,11 +31,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <>{person.name}<br /></>)}
+      {persons.map((person) => <>{person.name} {person.number}<br /></>)}
     </div>
   )
 }
